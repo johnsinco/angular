33ee81f88fae415c5418f991a5647567d6155270
John Stewart <lead.j2ee.engineer@gmail.com>
2013-05-07 14:32:28 -0600

    initial commit oppfunds.com demo of angular

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angular::Application.config.secret_token = 'a9744aed7982617a5e421d351ae31d2a56e2c7ed6ae788feb98b817d2d1e29f96ec504916f5be48feab137ffaa9ad81340d34bdb3bba7b168f93ba08a6aee406'
